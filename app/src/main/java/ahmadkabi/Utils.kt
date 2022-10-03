aff501379431ff33881cafc9c035eba512bf4176
ahmadkabi <qolamunapp@gmail.com>
2022-10-03 13:48:19 +0700

    add story

---
+++ app/src/main/java/ahmadkabi/Utils.kt
@@ -0,0 +1,50 @@
+package ahmadkabi
+
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val FILENAME_FORMAT = "dd-MMM-yyyy"
+
+val timeStamp: String = SimpleDateFormat(
+    FILENAME_FORMAT,
+    Locale.US
+).format(System.currentTimeMillis())
+
+fun createCustomTempFile(context: Context): File {
+    val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(timeStamp, ".jpg", storageDir)
+}
+
+fun uriToFile(selectedImg: Uri, context: Context): File {
+    val contentResolver: ContentResolver = context.contentResolver
+    val myFile = createCustomTempFile(context)
+
+    val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+    val outputStream: OutputStream = FileOutputStream(myFile)
+    val buf = ByteArray(1024)
+    var len: Int
+    while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+    outputStream.close()
+    inputStream.close()
+
+    return myFile
+}
+
+fun View.visible(){
+    visibility = VISIBLE
+}
+
+fun View.gone(){
+    visibility = GONE
+}
--- app/src/main/java/ahmadkabi/storyapp/AddStoryActivity.kt
+++ app/src/main/java/ahmadkabi/storyapp/AddStoryActivity.kt
@@ -1,20 +1,35 @@
 package ahmadkabi.storyapp
 
+import ahmadkabi.createCustomTempFile
+import ahmadkabi.gone
 import ahmadkabi.storyapp.databinding.ActivityAddStoryBinding
-import ahmadkabi.storyapp.databinding.ActivityDetailBinding
-import ahmadkabi.storyapp.databinding.ActivityHomeBinding
+import ahmadkabi.uriToFile
+import ahmadkabi.visible
+import android.Manifest
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.io.File
 
 class AddStoryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddStoryBinding
+    private lateinit var currentPhotoPath: String
+    private var getFile: File? = null
+
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    private val REQUEST_CODE_PERMISSIONS = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +37,111 @@ class AddStoryActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_add_story)
 
         binding.imgBack.setOnClickListener { finish() }
-        Glide
-            .with(this)
-            .load("https://story-api.dicoding.dev/images/stories/photos-1664615087142_m3gc6pE4.jpg")
-//            .transform(
-//                CenterCrop(),
-//                RoundedCorners(resources.getDimensionPixelSize(R.dimen.dp_30))
-//            )
-            .into(binding.imgStory)
+        binding.btnCamera.setOnClickListener { startTakePhoto() }
+        binding.btnGallery.setOnClickListener { startGallery() }
+        binding.imgCancel.setOnClickListener {
+            getFile = null
+            binding.imgStory.setImageBitmap(null)
+            binding.imgCancel.gone()
+
+            binding.btnCamera.visible()
+            binding.btnGallery.visible()
+            binding.btnMake.gone()
+        }
+
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
 
     }
 
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@AddStoryActivity,
+                "ahmadkabi.storyapp",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            getFile = myFile
+
+            val result = BitmapFactory.decodeFile(getFile?.path)
+
+            binding.imgStory.setImageBitmap(result)
+            binding.imgCancel.visible()
+            binding.btnCamera.gone()
+            binding.btnGallery.gone()
+            binding.btnMake.visible()
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+
+            val myFile = uriToFile(selectedImg, this@AddStoryActivity)
+
+            getFile = myFile
+
+            binding.imgStory.setImageURI(selectedImg)
+            binding.imgCancel.visible()
+            binding.btnCamera.gone()
+            binding.btnGallery.gone()
+            binding.btnMake.visible()
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.no_camera_permission),
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
 
     companion object {
         fun newIntent(context: Context): Intent {
