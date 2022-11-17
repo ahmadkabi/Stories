d8239a41b71b5b8ca9d95fd94e5729af96400fcd
ahmadkabi <qolamunapp@gmail.com>
2022-11-17 18:12:10 +0700

    pagination

--- app/src/main/java/ahmadkabi/storyapp/data/source/remote/model/Story.kt
+++ app/src/main/java/ahmadkabi/storyapp/data/source/remote/model/Story.kt
@@ -1,32 +1,11 @@
 package ahmadkabi.storyapp.data.source.remote.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
-
-@Entity(tableName = "story")
 data class Story(
-
-    @PrimaryKey
-    @field:SerializedName("id")
     val id: String,
-
-    @field:SerializedName("name")
     val name: String,
-
-    @field:SerializedName("description")
     val description: String,
-
-    @field:SerializedName("photoUrl")
     val photoUrl: String,
-
-    @field:SerializedName("createdAt")
     val createdAt: Any,
-
-    @field:SerializedName("lat")
     val lat: Float?,
-
-    @field:SerializedName("lon")
     val lon: Float?
-
 )
--- app/src/main/java/ahmadkabi/storyapp/data/source/remote/model/StoryResponse.kt
+++ app/src/main/java/ahmadkabi/storyapp/data/source/remote/model/StoryResponse.kt
@@ -1,20 +1,7 @@
 package ahmadkabi.storyapp.data.source.remote.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
-
-@Entity(tableName = "StoryResponse")
 data class StoryResponse(
-
-    @PrimaryKey
-    @field:SerializedName("error")
     val error: Boolean,
-
-    @field:SerializedName("message")
     val message: String,
-
-    @field:SerializedName("listStory")
     val listStory: List<Story>
-
 )
