6a717f9f070b5a0c0f3479396f5a354248381cd4
ahmadkabi <qolamunapp@gmail.com>
2024-02-10 06:34:13 +0700

    Add Core Modeul

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "ahmadkabi.stories.domain"
+    namespace = "ahmadkabi.stories.core"
     compileSdk = 34
 
     defaultConfig {
@@ -36,5 +36,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
