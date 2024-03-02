b9111074c3413a3ead75ae7a017ae58ac3804125
ahmadkabi <qolamunapp@gmail.com>
2024-03-02 09:46:39 +0700

    Abstracting project structure

--- core/src/main/java/ahmadkabi/stories/core/data/source/remote/Mapper.kt
+++ core/src/main/java/ahmadkabi/stories/core/data/source/remote/Mapper.kt
@@ -1,7 +1,9 @@
 package ahmadkabi.stories.core.data.source.remote
 
+import ahmadkabi.stories.core.data.source.remote.model.AddStoryResponse
 import ahmadkabi.stories.core.data.source.remote.model.LoginResult
 import ahmadkabi.stories.core.data.source.remote.model.RegisterResponse
+import ahmadkabi.stories.domain.model.AddStory
 import ahmadkabi.stories.domain.model.Register
 import ahmadkabi.stories.domain.model.User
 
@@ -12,4 +14,8 @@ fun LoginResult.toUserModel() = User(
 
 fun RegisterResponse.toRegister() = Register(
     isError = error
+)
+
+fun AddStoryResponse.toAddStory() = AddStory(
+    isSuccess = !error
 )
