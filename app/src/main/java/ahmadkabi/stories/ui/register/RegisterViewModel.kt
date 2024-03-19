5a971aae2c1a18156ea82a477b3e8975b3c14019
ahmadkabi <ahmadkabi.dev@gmail.com>
2024-03-19 07:07:36 +0700

    Update import

--- app/src/main/java/ahmadkabi/stories/ui/register/RegisterViewModel.kt
+++ app/src/main/java/ahmadkabi/stories/ui/register/RegisterViewModel.kt
@@ -1,16 +1,15 @@
 package ahmadkabi.stories.ui.register
 
 import ahmadkabi.stories.core.data.StoryRepository
+import ahmadkabi.stories.core.data.source.remote.ApiResponse
+import ahmadkabi.stories.core.data.source.remote.model.RegisterBody
+import ahmadkabi.stories.domain.model.Register
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import ahmadkabi.stories.core.data.source.remote.model.RegisterResponse
 import retrofit2.HttpException
-import ahmadkabi.stories.core.data.source.remote.ApiResponse
-import ahmadkabi.stories.core.data.source.remote.model.RegisterBody
-import ahmadkabi.stories.domain.model.Register
 
 class RegisterViewModel(private val storyRepository: StoryRepository) : ViewModel() {
 
