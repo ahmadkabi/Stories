9cb69673e0230b5c5ec4080cf40f6805ccc018fa
ahmadkabi <ahmadkabi.dev@gmail.com>
2024-06-30 07:28:46 +0700

    update dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
     /*todo*/
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
