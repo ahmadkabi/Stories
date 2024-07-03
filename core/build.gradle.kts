b5887d6b3e2bc33bc7a32131609d5cb274196ba9
ahmadkabi <ahmadkabi.dev@gmail.com>
2024-07-04 06:01:31 +0700

    update dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+    implementation("androidx.paging:paging-runtime-ktx:3.3.0")
 
 }
