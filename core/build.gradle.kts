46b34e8825f496408ebbb2690dbfc3cb20710713
ahmadkabi <ahmadkabi.dev@gmail.com>
2024-07-07 09:24:46 +0700

    remove todo

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
-    /*todo*/
+
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
