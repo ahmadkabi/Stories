4af82162401abc568c82ffb2b67d692483defae7
ahmadkabi <ahmadkabi.dev@gmail.com>
2024-09-15 06:48:28 +0700

    Add recyclerview adapter for stories with map

---
+++ app/src/main/java/ahmadkabi/stories/ui/map/StoriesInMapAdapter.kt
@@ -0,0 +1,80 @@
+package ahmadkabi.stories.ui.map
+
+import ahmadkabi.stories.R
+import ahmadkabi.stories.databinding.ItemStoryBinding
+import ahmadkabi.stories.domain.model.Story
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class StoriesInMapAdapter :
+    PagingDataAdapter<Story, StoriesInMapAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemStoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val data = getItem(position)
+        if (data != null) {
+            holder.bind(data)
+        }
+    }
+
+    inner class MyViewHolder(private val binding: ItemStoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Story) {
+
+
+            Glide
+                .with(itemView.context)
+                .load(data.photoUrl)
+                .transform(
+                    CenterCrop(),
+                    RoundedCorners(itemView.context.resources.getDimensionPixelSize(R.dimen.dp_30))
+                )
+                .into(binding.ivItemPhoto)
+
+            binding.txAvatar.text = data.name[0].toString().uppercase()
+            binding.tvItemName.text = data.name
+
+            itemView.setOnClickListener {
+                val optionsCompat: ActivityOptionsCompat =
+                    ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        itemView.context as Activity,
+                        Pair(binding.ivItemPhoto, "photo"),
+                        Pair(binding.llUser, "user")
+                    )
+                listener.onItemClickListener(data, optionsCompat)
+            }
+
+        }
+    }
+
+    lateinit var listener: ItemListener
+
+    interface ItemListener {
+        fun onItemClickListener(item: Story, optionsCompat: ActivityOptionsCompat)
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Story>() {
+            override fun areItemsTheSame(oldItem: Story, newItem: Story): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Story, newItem: Story): Boolean {
+                return oldItem.id == newItem.id
+            }
+        }
+    }
+}
